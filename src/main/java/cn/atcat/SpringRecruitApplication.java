868b6843dc6f65f04f9663a428ad93c8eecf2d4c
yanqing8 <2325552660@qq.com>
2023-11-25 16:00:18 +0800

    后端初始化

---
+++ src/main/java/cn/atcat/SpringRecruitApplication.java
@@ -0,0 +1,13 @@
+package cn.atcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecruitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/atcat/SpringRecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.atcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
